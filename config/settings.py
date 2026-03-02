fee5605b48ec2c3d16b663eaa1d1e0b806d7653e
Wajahat Hassan <wajahathassan699@gmail.com>
2026-03-02 10:43:17 -0500

    devops changes

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zhmce$x$m7@-9c*la4fx=h1#)3-o^#nz(5!1o@t363pki!em)p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app', 'elitesoftwarementorship.com']
 
 
 # Application definition
