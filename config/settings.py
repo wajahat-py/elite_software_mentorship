74d4f17916b1d96a74a2c67e508047db2e40439a
Wajahat Hassan <wajahathassan699@gmail.com>
2026-03-02 10:49:52 -0500

    cccc

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zhmce$x$m7@-9c*la4fx=h1#)3-o^#nz(5!1o@t363pki!em)p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app', 'elitesoftwarementorship.com']
 
@@ -116,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
