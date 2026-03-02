6fbb1655b1440695598f21070a3fda8e4c81a7f5
Wajahat Hassan <wajahathassan699@gmail.com>
2026-03-02 11:35:26 -0500

    sss

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zhmce$x$m7@-9c*la4fx=h1#)3-o^#nz(5!1o@t363pki!em)p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app', 'elitesoftwarementorship.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'elitesoftwarementorship.com']
 
 
 # Application definition
